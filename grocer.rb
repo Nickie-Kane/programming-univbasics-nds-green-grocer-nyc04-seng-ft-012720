0e0512efa22a4ba9824ae81f7d1060f3e6da170e
nickie kane <kameo712@gmail.com>
2020-01-06 01:11:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,10 +46,16 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  counter = 0 
+  i = 0 
   binding.pry
-  while counter < coupons.length do 
-    
+  while i < coupons.length do 
+    cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
+    coupon_item_name = "#{coupons[i][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[i][:num]
+      if cart_item_with_coupon 
+        cart_item_with_coupon[:count] += coupons[i][:num]
+        cart_item[:counter]
   end
 end
 
