d7380c8490d2c134271072793bf0ff3030803c3c
nickie kane <kameo712@gmail.com>
2020-01-06 00:52:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,10 +28,12 @@ def consolidate_cart(cart)
      if cart_item != nil 
        cart_item[:count] += 1  
      else 
-       cart_item[:item] = cart[counter][:item]
-       cart_item[:price] = cart[counter][:price]
-       cart_item[:clearance] = cart[counter][:clearance]
-       cart_item[:count] = 1
+       cart_item = {
+          cart_item[:item] = cart[counter][:item]
+          cart_item[:price] = cart[counter][:price]
+          cart_item[:clearance] = cart[counter][:clearance]
+          cart_item[:count] = 1
+       }
        new_cart << cart_item
      end
   counter += 1
